4c50ee67a3d55c2a8fc200984101eace1b6c9ebf
MillardFillmoreEsquire <MillardFillmoreEsquire@4a2dc538-bfd4-0ec6-af25-5c431d2cb63f>
2011-04-11 02:57:19 +0000

    timer_sleep no longer busy waits

--- trunk/threads/thread.h
+++ trunk/threads/thread.h
@@ -88,7 +88,7 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
-    uint64_t sleepytime;
+    int64_t sleepytime;
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
