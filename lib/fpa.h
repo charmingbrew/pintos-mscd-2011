5bce49a5567d16f1d4582b756fa44f09747b89a8
daneelshof@gmail.com <daneelshof@gmail.com@4a2dc538-bfd4-0ec6-af25-5c431d2cb63f>
2011-04-15 02:26:17 +0000

    Added lib/fpa.h

---
+++ lib/fpa.h
@@ -0,0 +1,70 @@
+#ifndef _LIB_FPA_H
+#define _LIB_FPA_H
+
+/* Helper functions for Fixed-Point Arithmetic */
+
+#define FPA_59_60       16110
+#define FPA_1_60        273
+#define FPA_F           16384
+
+int64_t
+conv_to_fpa (int64_t num, int64_t denom )
+{
+    return (num * FPA_F)/denom;
+}
+
+int64_t
+conv_from_fpa_nearest (int fpa)
+{
+    return (fpa + (FPA_F / 2)) / FPA_F;
+}
+
+int
+mult_fpa_integer (int64_t fpa, int64_t integer)
+{
+    return fpa * integer;
+}
+
+int64_t
+mult_fpa_fpa (int64_t fpa1, int64_t fpa2)
+{
+    return (fpa1 * fpa2 / FPA_F);
+}
+
+int64_t
+div_fpa_integer (int64_t fpa, int64_t integer)
+{
+    return fpa / integer;
+}
+
+int64_t
+div_fpa_fpa (int64_t fpa1, int64_t fpa2)
+{
+    return (fpa1 * FPA_F / fpa2);
+}
+
+int64_t
+sub_fpa_integer (int64_t fpa, int64_t integer)
+{
+    return fpa - integer * FPA_F;
+}
+
+int64_t
+sub_fpa_fpa (int64_t fpa1, int64_t fpa2)
+{
+    return fpa1 - fpa2;
+}
+
+int64_t
+add_fpa_integer (int64_t fpa, int64_t integer)
+{
+    return fpa + integer * FPA_F;
+}
+
+int64_t
+add_fpa_fpa (int64_t fpa1, int64_t fpa2)
+{
+    return fpa1 + fpa2;
+}
+
+#endif /* _LIB_FPA_H */
